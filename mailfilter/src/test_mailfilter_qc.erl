abf7093b3123f55045f0aa95b5d40ff0d8131607
Matias Ebbesen <matias@haeb.dk>
2020-11-01 18:11:49 +0100

    Tests work.

--- mailfilter/src/test_mailfilter_qc.erl
+++ mailfilter/src/test_mailfilter_qc.erl
@@ -7,62 +7,112 @@
 % 'test_'.
 % But you MUST have a module (this file) called test_mailfilter.
 -include_lib("eqc/include/eqc.hrl").
+
+-include("apqc_statem.hrl").
+
+-behaviour(apqc_statem).
+
+-export([command/1,
+         initial_state/0,
+         next_state/3,
+         postcondition/3,
+         precondition/2]).
+
 -compile(export_all).
+
 %test_all() -> eunit:test(register_test_(), [verbose]).
 
 -type mail() :: any().
+
 -type data() :: any().
+
 -type label() :: any().
--type result() :: {done, data()} | inprogress . -type labelled_result() :: {label(), result()}. -type filter_result() :: {just, data()}
-| {transformed, mail()}
-| unchanged
-| {both, mail(), data()}.
--type filter_fun() :: fun( (mail(), data()) -> filter_result() ).
 
--type filter() :: {simple, filter_fun()} | {chain, list(filter())}
-| {group, list(filter()), merge_fun() } | {timelimit, timeout(), filter()}.
--type merge_fun() :: fun( (list(filter_result() | inprogress)) -> filter_result() | continue ).
+-type result() :: {done, data()} | inprogress.
 
-results() -> eqc_gen:elements([{unchanged, {just, truth}, {transformed, newmail}, {both, data, mail}}]).
+-type labelled_result() :: {label(), result()}.
 
--type generator() :: fun(() -> filter_fun() ).
+-type filter_result() :: {just, data()} |
+                         {transformed, mail()} |
+                         unchanged |
+                         {both, mail(), data()}.
+
+-type filter_fun() :: fun((mail(),
+                           data()) -> filter_result()).
+
+-type filter() :: {simple, filter_fun()} |
+                  {chain, [filter()]} |
+                  {group, [filter()], merge_fun()} |
+                  {timelimit, timeout(), filter()}.
+
+-type merge_fun() :: fun(([filter_result() |
+                           inprogress]) -> filter_result() | continue).
+
+results() ->
+    eqc_gen:elements([{unchanged,
+                       {just, truth},
+                       {transformed, newmail},
+                       {both, data, mail}}]).
+
+-type generator() :: fun(() -> filter_fun()).
 
 -spec wellbehaved_filter_fun() -> filter_fun().
-wellbehaved_filter_fun() -> 
-    ?LET(R, results(),
-         fun(_M, _D) -> R end).
 
-filter_kind() -> eqc_gen:elements([{unchanged, {just, truth}, {transformed, newmail}, {both, data, mail}}]).
+wellbehaved_filter_fun() ->
+    ?LET(R, (results()), fun (_M, _D) -> R end).
+
+filter_kind() ->
+    eqc_gen:elements([{unchanged,
+                       {just, truth},
+                       {transformed, newmail},
+                       {both, data, mail}}]).
 
 -spec filter(generator()) -> filter().
-filter(FunGen) -> 
-    ?LET(R, FunGen,
-    oneof([{simple, R}])).
 
-prop_mail_is_sacred() ->
-    ?FORALL(T, bst(atom_key(), int_value()),
-            valid(T)).
+filter(FunGen) ->
+    ?LET(R, FunGen, (oneof([{simple, R}]))).
 
 mail() -> eqc_gen:utf8().
 
--type model_state() ::
-#{ mails := [any()] }.
+-type model_state() :: #{ms := pid(), mails := [any()]}.
 
 -spec initial_state() -> model_state().
-initial_state() -> #{mails => []}.
 
-command(S) ->
-       oneof(
-         [{call,mailfilter, add_mail, [S, mail()]}, 
-        {call,mailfilter,unregister,[name()]}, 
-        {call,mailfilter,spawn,[]}]).
+initial_state() -> #{ms => none, mails => []}.
 
+command(#{ms := none}) ->
+    return({call, mailfilter, start, [infinite]});
+command(#{ms := MS}) ->
+    oneof([{call, mailfilter, add_mail, [MS, mail()]}]).
 
-prop_erase()->
-        ?FORALL(D, dict(),
-        ?FORALL(K, key_from(D),
-        beginDict=eval(D),
-        equals(model(dict:erase(K,Dict)),
-        model_erase(K,model(Dict)))end)).
+next_state(#{mails := Mails} = S, _V,
+           {call, mailfilter, add_mail, [_MS, Mail]}) ->
+    S#{mails := [Mail | Mails]};
+next_state(S, _V, _) -> S.
 
+precondition(_S, {call, _, _, _}) -> true.
+
+postcondition(_S, {call, _, _, _}, _R) -> true.
+
+prop_registration() ->
+    ?FORALL(Cmds, (commands(?MODULE)),
+            begin
+                {H, S, R} = Result = run_commands(?MODULE, Cmds),
+                #{ms := MS, mails := Mails} = S,
+                Labelled = case MS of 
+                        none -> [];
+                        MS -> mailfilter:stop(MS)
+                end,
+                check_commands(Cmds, Result),
+                same_mails(Mails, Labelled)
+            end).
+
+same_mails(List, Labelled) ->
+    lists:sort(List) =:=
+        lists:sort([Mail || {Mail, _} <- Labelled]).
+
+check_commands(Cmds, {_,_,Res} = HSRes) ->
+    pretty_commands(?MODULE, Cmds, HSRes,
+                    aggregate(command_names(Cmds),
+                              equals(Res, ok))).
 
